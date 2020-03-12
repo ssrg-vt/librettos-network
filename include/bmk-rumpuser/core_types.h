8c451ec008af17e5705f07d9f7df3f7fa9d70aa5
Ruslan Nikolaev <nruslandev@gmail.com>
2020-03-11 23:55:33 -0400

    Updating type definitions

--- include/bmk-rumpuser/core_types.h
+++ include/bmk-rumpuser/core_types.h
@@ -29,6 +29,13 @@
  * in a can of worms, we define just those constructs here.
  */
 
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+
 #ifdef _LP64
 typedef long int64_t;
 typedef unsigned long uint64_t;
